3a93536f2c29c46e37692b8d80757ccd29bb26ae
iamalolz <92262166+iamalolz@users.noreply.github.com>
2024-04-15 14:38:39 +0800

    esrsresrsresrs

--- Question4.java
+++ Question4.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
-public class Question1 {
+public class Question4 {
   public static void main(String[] args) {
     Scanner obj = new Scanner(System.in);
     System.out.println("Enter an integer: ");
     int in = obj.nextInt();
-    
+
     for (int i = in; i > 0; i--) {
       for (int j = 0; j < i; j++) {
         System.out.print("*");
