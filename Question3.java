9dc0a060bce86154047231db3ef203514e86ca93
iamalolz <92262166+iamalolz@users.noreply.github.com>
2024-04-15 14:21:39 +0800

    attempt 2

--- Question3.java
+++ Question3.java
@@ -4,7 +4,6 @@ public class Question3
 {
   public static void main(String[] args)
   {
-    Scanner obj = new Scanner(System.in);
     System.out.println("Enter an integer: ");
     int in = obj.nextInt();
     System.out.println("Output: " + in*in);
