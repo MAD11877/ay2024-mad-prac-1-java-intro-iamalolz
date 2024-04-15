1e4481e73506b64777753a2088288d2679f2b746
iamalolz <92262166+iamalolz@users.noreply.github.com>
2024-04-15 14:46:11 +0800

    maybe

--- Question2.java
+++ Question2.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class Question2 {
   public static void main(String[] args) {
     Scanner obj = new Scanner(System.in); 
-    System.out.println("Enter height: ");
     Double height = obj.nextDouble();
-    System.out.println("Enter weight: ");
     Double weight = obj.nextDouble();
     Double bmi = weight / (height * height);
     System.out.println(bmi);
